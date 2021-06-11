94d7500b831fa3a06e79b92da895740901a6def7
reings23 <dhruv23mathur@gmail.com>
2021-06-12 01:20:42 +0530

    add 01-06

---
+++ 01-06-21/reverse.js
@@ -0,0 +1,5 @@
+var a=[10,20,30,40,50];
+var b=[];
+ for(var i= a.length-1;i >= 0;i-- )
+   b.push(a[i]);
+console.log(b);
---
+++ 01-06-21/while.js
@@ -0,0 +1,9 @@
+var a=[10,20,30,40,50];
+var b=[];
+var i=4;
+while(i>=0)
+{
+    b.push(a[i]);
+    i--;
+}
+console.log(b)
