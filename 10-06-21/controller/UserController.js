44d14bcbfa9779ccfb9bd2d188e0261c3241d6ad
reings23 <dhruv23mathur@gmail.com>
2021-06-19 09:39:23 +0530

    node&express

---
+++ 10-06-21/controller/UserController.js
@@ -0,0 +1,98 @@
+const User = require("../models/User");
+const mongoose = require("mongoose");
+const JWT = require("jsonwebtoken");
+
+exports.signUp = (req, res) => {
+  let { firstName, lastName, email, password } = req.body;
+  let user = new User({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+  user
+    .save()
+    .then(() => {
+      const token = getToken(user);
+      return res.status(200).send({ user, token });
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).send("ERROR");
+    });
+};
+
+exports.login = (req, res) => {
+  let { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      console.info(`User with email: ${email} was successfully found`);
+      if (password === user.password) {
+        const token = getToken(user);
+        console.info("Login successful");
+        return res.status(200).send({ user, token });
+      }
+      console.warn("Password incorrect");
+      return res.status(401).send("Password was incorrect");
+    })
+    .catch((error) => {
+      console.error(`User with email: ${email} does not exist!`);
+      return res.status(404).send(`User with email: ${email} doesn't exist!`);
+    });
+};
+
+exports.getUserById = (req, res) => {
+  let id = req.params.id;
+  id = mongoose.Types.ObjectId(id);
+  User.findOne({ _id: id })
+    .then((user) => {
+      if (user) {
+        console.info("User found");
+        return res.status(200).send(user);
+      }
+      console.error("User was not found!");
+      return res.status(404).send("NOT FOUND");
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).send("ERROR");
+    });
+};
+
+exports.updateUser = (req, res) => {
+  let id = mongoose.Types.ObjectId(req.params.id);
+  let { firstName, lastName } = req.body;
+  User.updateOne({ _id: id }, { $set: { firstName, lastName } })
+    .then((updateResult) => {
+      if (
+        updateResult.nModified >= 0 &&
+        updateResult.n >= 1 &&
+        updateResult.ok >= 1
+      ) {
+        console.info("User was successfully updated.");
+        return res.status(200).send("User was successfully updated.");
+      }
+      console.error(`User with ID: ${id} doesn't exist!`);
+      return res.status(404).send(`User with ID: ${id} doesn't exist!`);
+    })
+    .catch((error) => {
+      console.error("There was an error while updating user.", error);
+      return res.status(500).send("ERROR");
+    });
+};
+
+exports.isValid = (req, res) => {
+  return res.status(200).send("Token valid");
+};
+
+const getToken = (user) => {
+  return JWT.sign(
+    {
+      email: user.email,
+    },
+    "JIETSecretKey",
+    {
+      expiresIn: "1h",
+    }
+  );
+};
--- 10-06-21/routes/UserRoute.js
+++ 10-06-21/routes/UserRoute.js
@@ -1,77 +1,16 @@
 const express = require("express");
-const User = require("../models/User");
 const router = express();
-const mongoose = require("mongoose");
+const userController = require("../controller/UserController");
+const tokenMiddleware = require("../middleware/isTokenValid")
 
-router.post("/signup", (req,res) =>{
-    let{ firstName, lastName, email, password } = req.body;
-    let user = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-    });
-    user.save()
-    .then(() => {
-        return res.status(200).send(user)
-    })
-    .catch((error) => {
-        console.error(error);
-        return res.status(500).send("ERROR");
-    });
-});
-router.post("/login", (req, res) => {
-    let { email, password } = req.body;
-    User.findOne({ email: email })
-      .then((User) => {
-        console.info(`User with email: ${email} was successfully found`);
-        if (password === user.password) {
-          console.info("Login successful");
-          return res.status(200).send(user);
-        }
-        console.warn("Password incorrect");
-        return res.status(401).send("Password was incorrect");
-      })
-      .catch((error) => {
-        console.error(`User with email: ${email} does not exist!`);
-        return res.status(404).send(`User with email: ${email} doesn't exist!`);
-      });
-  });
-  router.get("/:id", (req, res) => {
-    let id = req.params.id;
-    id = mongoose.Types.ObjectId(id);
-    User.findOne({ _id: id })
-      .then((User) => {
-        if (User) {
-          console.info("User found");
-          return res.status(200).send(User);
-        }
-        console.error("User was not found!");
-        return res.status(404).send("NOT FOUND");
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.status(500).send("ERROR");
-      });
-  });
-  
-  router.put("/update/:id", (req, res) => {
-    let { email, password, firstName, lastName } = req.body;
-    let id = req.params.id;
-    id = mongoose.Types.ObjectId(id);
-    User.updateOne(
-      { _id: id },
-      { $set: { email, password, firstName, lastName } }
-    )
-      .then(() => {
-        console.info("Update successful");
-        return res.status(200).send({ email, password, firstName, lastName });
-      })
-      .catch((error) => {
-        console.error("There was an error while updating!");
-        return res.status(500).send("There was an error while updating the user");
-      });
-  });
-  
-  module.exports = router;
-  
+router.post("/signup", userController.signUp);
+
+router.post("/login", userController.login);
+
+router.get("/user/:id", userController.getUserById);
+
+router.put("/update/:id", tokenMiddleware.isTokenValid, userController.updateUser);
+
+router.get("/token", tokenMiddleware.isTokenValid, userController.isValid);
+
+module.exports = router;
