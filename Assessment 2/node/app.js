28935c10446cf4da30814d80794554411cac9962
reings23 <dhruv23mathur@gmail.com>
2021-06-30 14:50:21 +0530

    assessment 2

--- Assessment 2/node/app.js
+++ Assessment 2/node/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const {Router} = require("express");
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
@@ -16,6 +17,29 @@ app.get("/data", (req,res) => {
          return res.status(404).send("error");
      });
 });
+
+Router.get("/getdata/:postId", (req, res) => {
+    let { postID } = req.params;
+    postID = mongoose.Types.ObjectId(postID);
+    axios.get(`https://jsonplaceholder.typicode.com/${postId}/comments`);
+    Posts.find({ _id: postId })
+      .then((post) => {
+        console.info("Post has been found");
+        return Post.find({ commets })
+          .then((post) => {
+            console.info("AVALAIBLE Comments ARE:");
+            return res.status(200).send({ comments });
+          })
+          .catch(() => {
+            console.error(" error");
+            return res.status(500).send(" error");
+          });
+      })
+      .catch(() => {
+        console.error("error has occured");
+        return res.status(500).send("No theater Found");
+      });
+    });
 
 
 
